fda81af7b5e4f4685651d80d8790df8bdfcf9860
Pushkar Upadhyay <pushkar.softwaredev@gmail.com>
2025-04-29 22:31:04 +0530

    Group Created

--- ping.js
+++ ping.js
@@ -2,12 +2,18 @@ const ping = require("ping");
 const fs = require("fs");
 
 let ipList = require("./ip_list.json");
+let groupConfig = require("./group_config.json");
 let statusMap = {};
 
 async function updateStatus() {
-  for (let ip of ipList) {
+  for (let ip of ipList.map(obj => typeof obj === 'string' ? obj : obj.ip)) {
     const res = await ping.promise.probe(ip, { timeout: 1 });
-    statusMap[ip] = res.alive ? "Online" : "Offline";
+    statusMap[ip] = statusMap[ip] || {};
+    statusMap[ip].status = res.alive ? "Online" : "Offline";
+    let ipObj = ipList.find(i => typeof i === 'object' && i.ip === ip);
+    if (ipObj && ipObj.location) {
+      statusMap[ip].location = ipObj.location;
+    }
   }
 }
 
@@ -15,8 +21,18 @@ function getStatus() {
   return statusMap;
 }
 
-// Run on interval
-setInterval(updateStatus, 10000); // every 10 sec
+function getStatusByGroup(groupId) {
+  const group = groupConfig[groupId];
+  if (!group) return null;
+
+  const result = {};
+  for (let ip of group.ips) {
+    if (statusMap[ip]) result[ip] = statusMap[ip];
+  }
+  return result;
+}
+
+setInterval(updateStatus, 10000);
 updateStatus();
 
-module.exports = { getStatus };
+module.exports = { getStatus, getStatusByGroup };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const { getStatus } = require("./ping");
+const { getStatus, getStatusByGroup } = require("./ping");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +11,18 @@ app.use(express.static("public"));
 
 app.get("/status", (req, res) => {
   res.json(getStatus());
+});
+
+app.get("/status/:groupId", (req, res) => {
+  const data = getStatusByGroup(req.params.groupId);
+  if (!data) {
+    return res.status(404).json({ error: "Group not found" });
+  }
+  res.json(data);
+});
+
+app.get("/share/:groupId", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "share.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
